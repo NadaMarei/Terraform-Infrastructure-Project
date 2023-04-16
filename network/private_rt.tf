6e03fab38bfe5f6709be48d6c2b5f48c000a13e7
NadaMarey <nadamarey@localhost.localdomain>
2023-04-17 00:16:04 +0200

    update

--- network/private_rt.tf
+++ network/private_rt.tf
@@ -3,6 +3,12 @@
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.my_vpc.id
 
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+
   tags = {
     Name = "private-rt"
   }
