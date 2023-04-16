6e17da29fda5132cf858f75ce6aad1264dea1345
NadaMarey <nadamarey@localhost.localdomain>
2023-04-16 23:56:38 +0200

    update

--- app_sg.tf
+++ app_sg.tf
@@ -18,4 +18,11 @@ resource "aws_security_group" "app_sg" {
     protocol = "tcp"
     cidr_blocks = [module.network.vpc_cidr]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
 }
--- bastion_sg.tf
+++ bastion_sg.tf
@@ -11,4 +11,11 @@ resource "aws_security_group" "bastion_sg" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
 }
