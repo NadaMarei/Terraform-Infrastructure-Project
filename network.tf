6339d301a02062b6d1470bb41fd2ee2401561bab
NadaMarey <nadamarey@localhost.localdomain>
2023-04-16 21:19:08 +0200

    update

---
+++ network.tf
@@ -0,0 +1,10 @@
+module "network" {
+  source = "../network"
+  vpc_cidr = var.VPC_CIDR
+  public_subnet_1_cider = var.PUBLIC_SUBNET_1_CIDR
+  public_subnet_2_cider = var.PUBLIC_SUBNET_2_CIDR
+  private_subnet_1_cider = var.PRIVATE_SUBNET_1_CIDR
+  private_subnet_2_cider = var.PRIVATE_SUBNET_2_CIDR
+  az_1 = var.AZ_1
+  az_2 = var.AZ_2
+}
